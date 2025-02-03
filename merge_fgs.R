4a6fd0652f018d84d3bc69572e57603648d8dcb2
Liz McCleary <elizabethmccleary11@gmail.com>
2025-02-03 11:57:35 -0800

    Update merge_fgs

--- merge_fgs.R
+++ merge_fgs.R
@@ -3,13 +3,17 @@ library(readxl)
 library(dplyr)
 library(openxlsx)
 
-# Load 2024 point frame data and functional groups data
-point_frame <- read_excel("/Users/lizmccleary/Desktop/Garibaldi_Analysis-1/2024_point_frame.xlsx")
+# Load 2022 point frame data and functional groups data
+point_frame <- read_excel("/Users/lizmccleary/Desktop/Garibaldi_Analysis-1/2022_point_frame.xlsx")
 fgs <- read_excel("/Users/lizmccleary/Desktop/Garibaldi_Analysis-1/Species_Func_Groups.xlsx")
 
 # Add a column to track the original order
 point_frame <- point_frame %>%
   mutate(original_order = row_number())
+
+# Alter the Canopy Height column so that it is the same measurement of the 2024 data 
+point_frame <- point_frame %>%
+  mutate(CanopyHeight.mm. = CanopyHeight.mm. * 10) 
 
 # Perform the join between point_frame and fgs
 # Join on the correct species and subspecies columns
@@ -21,5 +25,5 @@ merged_data <- merged_data %>%
   arrange(original_order) %>%
   select(-original_order)
 
-# Write merged data to new sheet in 2024 point frame data 
-write.xlsx(merged_data, "/Users/lizmccleary/Desktop/Garibaldi_Analysis-1/2024_point_frame.xlsx", sheetName = "merged_data", append = TRUE)
+# Write merged data to new sheet in 2022 point frame data 
+write.xlsx(merged_data, "/Users/lizmccleary/Desktop/Garibaldi_Analysis-1/merged_data_2022.xlsx", sheetName = "merged_data_2022", append = TRUE)
