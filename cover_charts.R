dd25abbd9798c8b7d83087d0f201bb8c554d073e
Liz McCleary <elizabethmccleary11@gmail.com>
2025-02-03 11:18:25 -0800

    RPlot for relative cover percentage updated to only compared Top hits.

--- cover_charts.R
+++ cover_charts.R
@@ -12,9 +12,10 @@ merged_2024 <- read_excel(file_path_2024, sheet = 1)
 file_path_2022 <- "/Users/lizmccleary/Desktop/Garibaldi_Analysis-1/merged_data_2022.xlsx"
 merged_2022 <- read_excel(file_path_2022, sheet = 1)
 
-# Clean and process 2024 data by removing rows with NA values
+# Clean and process 2024 data by removing rows with NA values and only consider heights that are a Top hit
 data_clean_2024 <- merged_2024 %>%
   filter(!is.na(SPP), !is.na(CanopyHeight.mm.), !is.na(HitOrder), !is.na(SITE)) %>%
+  filter(HitOrder == "Top") %>%  
   mutate(Year = 2024)  # Add Year column to distinguish datasets
 
 # Clean and process 2022 data by removing rows with NA values
@@ -28,24 +29,24 @@ combined_data <- bind_rows(data_clean_2024, data_clean_2022)
 # Group by species, height, hit order, site, and year, and calculate percent cover
 percent_cover_data <- combined_data %>%
   group_by(SPP, SITE, CanopyHeight.mm., HitOrder, Year) %>%
-  summarise(CoverCount = n(), .groups = "drop") %>%
+  summarise(CoverCount = n(), .groups = "drop") %>% # CoverCount is the number of times a unique combination appears
   group_by(SITE, Year) %>%  # Group by site and year to calculate relative percent cover
-  mutate(TotalCover = sum(CoverCount)) %>%
+  mutate(TotalCover = sum(CoverCount)) %>% #TotalCover = the total number of hits within a specific site and year
   mutate(RelativePercentCover = (CoverCount / TotalCover) * 100) %>%
-  ungroup()  # Ungroup after calculation
+  ungroup()
 
-# Create the plot with relative percent cover and fixed y-axis scale
+# Create the plot with relative percent cover
 ggplot(percent_cover_data, aes(x = factor(CanopyHeight.mm.), y = RelativePercentCover, fill = factor(SPP))) +
   geom_bar(stat = "identity", position = "stack") +
-  facet_wrap(~ SITE + Year, scales = "fixed") +  # Ensure all sites use the same y-axis scale
+  facet_wrap(~ SITE + Year) +  
   labs(title = "Relative Percent Cover of Species by Height, Site Type, and Year", 
        x = "Canopy Height (mm)", 
        y = "Relative Percent Cover (%)") +
-  scale_fill_viridis(discrete = TRUE) +  # A color palette suitable for many categories
+  scale_fill_viridis(discrete = TRUE) + 
   theme_minimal() +
   theme(
-    axis.text.x = element_blank(),  # Remove x-axis text labels
-    axis.ticks.x = element_blank(),  # Remove x-axis tick marks
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),  
     strip.text = element_text(size = 12),
     axis.text = element_text(size = 10),
     axis.title = element_text(size = 12)
