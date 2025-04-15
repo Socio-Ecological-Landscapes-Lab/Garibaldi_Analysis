8da7185858384e4768673dcfdcda7df7f7f1eb70
Liz McCleary <elizabethmccleary11@gmail.com>
2025-04-15 09:00:09 -0700

    Add files via upload

--- clean_compile.R
+++ clean_compile.R
@@ -4,7 +4,7 @@ library(dplyr)
 library(openxlsx)
 
 # Load data into frame 
-data <- read_excel("/Users/lizmccleary/Desktop/Garibaldi_R/2024_point_frame_raw.xlsx")
+data <- read_excel("/Users/lizmccleary/Desktop/Garibaldi_Analysis-1/2024_point_frame_raw.xlsx")
 
 plot_naming <- function(data_column, orig_name1, new_name1){
   data_column <- as.character(data_column)
@@ -22,33 +22,49 @@ unique(data$SPP_sub)
 data$SPP <- plot_naming(data$SPP, "carmini.*", "poaspp")
 data$SPP <- plot_naming(data$SPP, "soli", "eriper")
 data$SPP <- plot_naming(data$SPP, "carmed", "carnig")
+data$SPP <- plot_naming(data$SPP, "moss.", "moss")
 
 data$SPP_sub <- plot_naming(data$SPP_sub, "carmini.*", "poaspp")
 data$SPP_sub <- plot_naming(data$SPP_sub, "soli", "eriper")
 data$SPP_sub <- plot_naming(data$SPP_sub, "carmed", "carnig")
+data$SPP_sub <- plot_naming(data$SPP_sub, "moss.", "moss")
 
 
 data$SPP <- data$SPP_sub
 
 # Write changes to the dataset
-# write.xlsx(data, file = "/Users/lizmccleary/Desktop/Garibaldi R/2024_point_frame_raw.xlsx", sheetName = "2024_pf_cleaned_FGs", append = TRUE)
+write.xlsx(data, file = "/Users/lizmccleary/Desktop/Garibaldi_Analysis-1/2024_point_frame_raw.xlsx", sheetName = "2024_pf_cleaned_FGs", append = TRUE)
 
 #Now that the data is clean, we can add functional groups 
 # Load 2022 point frame data and functional groups data
-point_frame <- read_excel("/Users/lizmccleary/Desktop/Garibaldi_Analysis-1/2022_point_frame_raw.xlsx")
+point_frame2022 <- read_excel("/Users/lizmccleary/Desktop/Garibaldi_Analysis-1/2022_point_frame_raw.xlsx")
 fgs <- read_excel("/Users/lizmccleary/Desktop/Garibaldi_Analysis-1/Species_Func_Groups.xlsx")
 
 # Add a column to track the original order
-point_frame <- point_frame %>%
-  mutate(original_order = row_number())
+point_frame2022 <- point_frame2022 %>%
+  dplyr::mutate(original_order = row_number())
 
 # Alter the Canopy Height column so that it is the same measurement of the 2024 data 
-point_frame <- point_frame %>%
-  mutate(CanopyHeight.mm. = CanopyHeight.mm. * 10) 
+point_frame2022 <- point_frame2022 %>%
+  dplyr::mutate(CanopyHeight.mm. = CanopyHeight.mm. * 10) 
+
+# Make sure DATE is character
+point_frame2022$DATE <- as.character(point_frame2022$DATE)
+
+# Replace "6/22" and similar with "2022"
+point_frame2022$DATE[point_frame2022$DATE == "6/22"] <- "2022"
+
+# Replace "9/26/22" with "2022"
+point_frame2022$DATE[grepl("^\\d{1,2}/\\d{1,2}/\\d{2}$", point_frame2022$DATE)] <- "2022"
+
+# Replace any "2019" with "2022"
+point_frame2022$DATE <- plot_naming(point_frame2022$DATE, "2019", "2022")
+
+# Keep last four digits of any unchanged DATE fields 
+point_frame2022$DATE <- gsub(".*(\\d{4})$", "\\1", point_frame2022$DATE)
 
 # Perform the join between point_frame and fgs
-# Join on the correct species and subspecies columns
-merged_data2022 <- point_frame %>%
+merged_data2022 <- point_frame2022 %>%
   left_join(fgs, by = c("SPP" = "SPP_code"), relationship = "many-to-many")
 
 # Arrange the data back in its original order
@@ -56,21 +72,19 @@ merged_data2022 <- merged_data2022 %>%
   arrange(original_order) %>%
   select(-original_order)
 
-# Write merged data to new sheet in 2022 point frame data 
-# write.xlsx(merged_data, "/Users/lizmccleary/Desktop/Garibaldi_Analysis-1/merged_data_2022.xlsx", sheetName = "merged_data_2022")
-
-
 # Load 2024 point frame data and functional groups data
-point_frame <- read_excel("/Users/lizmccleary/Desktop/Garibaldi_Analysis-1/2024_point_frame_raw.xlsx")
+point_frame2024 <- read_excel("/Users/lizmccleary/Desktop/Garibaldi_Analysis-1/2024_point_frame_raw.xlsx")
 fgs <- read_excel("/Users/lizmccleary/Desktop/Garibaldi_Analysis-1/Species_Func_Groups.xlsx")
 
 # Add a column to track the original order
-point_frame <- point_frame %>%
-  mutate(original_order = row_number())
+point_frame2024 <- point_frame2024 %>%
+  dplyr::mutate(original_order = row_number())
+point_frame2024 <- point_frame2024 %>%
+  dplyr::mutate(DATE = substr(DATE, nchar(DATE) - 3, nchar(DATE)))
+
 
 # Perform the join between point_frame and fgs
-# Join on the correct species and subspecies columns
-merged_data2024 <- point_frame %>%
+merged_data2024 <- point_frame2024 %>%
   left_join(fgs, by = c("SPP" = "SPP_code"), relationship = "many-to-many")
 
 # Arrange the data back in its original order
@@ -78,6 +92,9 @@ merged_data2024 <- merged_data2024 %>%
   arrange(original_order) %>%
   select(-original_order)
 
-#Append merged_data2024 to bottom of merged_data2022 and write to one dataset called ITEX_2025
-write.xlsx(merged_data, "/Users/lizmccleary/Desktop/Garibaldi_Analysis-1/ITEX_2025.xlsx", sheetName = "ITEX_2025")
+# Append merged_data2024 to merged_data2022 and remove unnecessary columns 
+combined_data <- bind_rows(merged_data2022, merged_data2024)%>%
+  dplyr::select(-filename, -...1, -SPP_sub, -TOMST, -Flag, -Flag.1, -Species, -Validated_23_08, -Description)
+
+write.xlsx(combined_data, "/Users/lizmccleary/Desktop/Garibaldi_Analysis-1/ITEX_2025.xlsx", sheetName = "ITEX_2025")
 
--- stat_ITEX2024.R
+++ stat_ITEX2024.R
@@ -2,6 +2,7 @@ setwd("~/Desktop/Garibaldi_Analysis-1")
 
 library(ggplot2)
 library(plyr)
+library(dplyr)
 library(tidyr)
 library(forcats)
 library(gridExtra)
@@ -10,11 +11,9 @@ library(readxl)
 library(vegan)
 library(cowplot)
 
-##################################################
-
 #Calculate changes in vegetation
-dataset <-read_xlsx("/Users/lizmccleary/Desktop/Garibaldi_Analysis-1/ITEX_merged_2025.xlsx")%>% 
-  group_by(SITE,DATE,TRTMT,PLOT,X,Y,SPECIES)%>% 
+dataset <-read_xlsx("/Users/lizmccleary/Desktop/Garibaldi_Analysis-1/ITEX_2025.xlsx")%>% 
+  group_by(SITE,DATE,TRTMT,PLOT,X,Y,SPP)%>% 
   mutate(position=case_when(HitOrder=="Bottom" ~ "understory",
                             HitOrder=="Top"~"canopy",
                             is.na(HitOrder)~"middle"))%>% 
@@ -30,36 +29,35 @@ allDataUnder<- dataset %>%
 allDataTopBottom <- dataset %>%
   filter(!position=="middle")
 
-##Top and bottom total species cover
+#Top and bottom total species cover
 coverTopBottom <- allDataTopBottom%>% 
-  select(SITE, TRTMT, PLOT, DATE, SPECIES, HitOrder)%>% 
-  mutate(SPECIES=as.factor(SPECIES))%>% 
-  group_by(DATE, SITE, TRTMT, PLOT, SPECIES) %>% 
+  select(SITE, TRTMT, PLOT, DATE, SPP, HitOrder)%>% 
+  mutate(SPP=as.factor(SPP))%>% 
+  group_by(DATE, SITE, TRTMT, PLOT, SPP) %>% 
   tally(name = "cover") %>%
   ungroup()
 
 coverCanopy <- allDataCanopy%>% 
-  select(SITE, TRTMT, PLOT, DATE, SPECIES, HitOrder)%>% 
-  mutate(SPECIES=as.factor(SPECIES))%>% 
-  group_by(DATE,SITE,TRTMT,PLOT, SPECIES)%>% 
+  select(SITE, TRTMT, PLOT, DATE, SPP, HitOrder)%>% 
+  mutate(SPP=as.factor(SPP))%>% 
+  group_by(DATE,SITE,TRTMT,PLOT, SPP)%>% 
   tally(name = "cover")%>% 
   mutate(cover = cover/100)%>%
   ungroup()
 
 coverUnder <- allDataUnder%>% 
-  select(SITE, TRTMT, PLOT, DATE, SPECIES, HitOrder)%>% 
-  mutate(SPECIES=as.factor(SPECIES))%>% 
-  group_by(DATE,SITE, TRTMT,PLOT, SPECIES)%>% 
+  select(SITE, TRTMT, PLOT, DATE, SPP, HitOrder)%>% 
+  mutate(SPP=as.factor(SPP))%>% 
+  group_by(DATE,SITE, TRTMT,PLOT, SPP)%>% 
   tally(name = "cover")%>%
   mutate(cover = cover/100)%>%
   ungroup()
 
-##Relative cover percentage and change in cover per species 
+#Relative cover percentage and change in cover per species 
 percent_cover_summary <- coverCanopy %>%
-  dplyr::group_by(DATE, SPECIES) %>%
+  dplyr::group_by(DATE, SPP) %>%
   dplyr::summarize(TotalPercentCover = sum(cover, na.rm = TRUE))
 
-# View the summary
 print(percent_cover_summary)
 
 #Use summary to identify which species appear in one year but not in the other
@@ -84,7 +82,7 @@ print("Species in 2024 but not in 2022:")
 print(species_in_2024_not_2022)
 
 
-ggplot(percent_cover_summary, aes(x = DATE, y = TotalPercentCover, color = SPECIES)) +
+ggplot(percent_cover_summary, aes(x = DATE, y = TotalPercentCover, color = SPP)) +
   geom_line() +
   geom_point() +
   labs(title = "Percent Cover Changes per Species Over Time",
@@ -98,17 +96,15 @@ ggplot(percent_cover_summary, aes(x = DATE, y = TotalPercentCover, color = SPECI
 
 SITES<- coverTopBottom %>% 
         filter(SITE %in% c("Salix", "Cassiope","Meadow"))%>% 
-        filter(!SPECIES %in% "carspp")%>%
-        select(DATE,SITE,TRTMT,PLOT, SPECIES, cover)%>% 
-        pivot_wider(names_from = SPECIES, values_from = cover)
+        filter(!SPP %in% "carspp")%>%
+        select(DATE,SITE,TRTMT,PLOT, SPP, cover)%>% 
+        pivot_wider(names_from = SPP, values_from = cover)
 
-#Remove columns with all 0 or NA values 
 #Swap all NA for zeros
 SITES[is.na(SITES)] <- 0
 
 
-###NMDS
-#Do NMDS for all sites
+###NMDS for all sites 
 year <- SITES [,1]
 SITE <- SITES[,2]
 TRTMT <- SITES [,3]
@@ -120,13 +116,6 @@ site.scrs <- as.data.frame(scores(veg.mds, display = "sites"))
 site.scrsSITES <- cbind(site.scrs, year, SITE, TRTMT)
 site.scrsSITES <- site.scrsSITES[-28,]
 
-
-#site.scrsSITES$site<- c("Salix", "Cassiope", "Meadow") #figure generated without this
-
-#fit environmental variables with envfit
-#env<- cbind(year, TRTMT)
-#fit<-envfit(veg.mds, env)
-
 en_coord_cont = as.data.frame(scores(fit, "vectors")) * ordiArrowMul(fit)
 en_coord_cat = as.data.frame(scores(fit, "factors")) * ordiArrowMul(fit)
 
@@ -135,15 +124,6 @@ en_coord_cat = as.data.frame(scores(fit, "factors")) * ordiArrowMul(fit)
 ggplot()+ 
   geom_point(data=site.scrsSITES, aes(NMDS1, NMDS2, colour = as.factor(SITE), shape=factor(TRTMT)),
              size=3) 
-#+
-  #geom_segment(aes(x = 0, y = 0, xend = NMDS1, yend = NMDS2), 
-               #data = en_coord_cont, size =1, alpha = 0.5, colour = "grey30") +
-  #geom_point(data = en_coord_cat, aes(x = NMDS1, y = NMDS2), 
-             #shape = "diamond", size = 4, alpha = 0.6, colour = "navy") +
-  #geom_text(data = en_coord_cat, aes(x = NMDS1, y = NMDS2+0.04), 
-            #label = row.names(en_coord_cat), colour = "navy", fontface = "bold") + 
-  #geom_text(data = en_coord_cont, aes(x = NMDS1, y = NMDS2), colour = "grey30", 
-            #fontface = "bold", label = row.names(en_coord_cont))
 
 #Run permanova
 veg.dist <- vegdist(veg, method="bray")
@@ -154,41 +134,32 @@ data(dune.env)
 
 ####CHANGE IN HEIGHT
 
-dataset <-read_xlsx("ITEX_merged_2025.xlsx")
-
 Height_ALL <- dataset %>%
   filter(SITE %in% c("Salix", "Meadow", "Cassiope")) %>%
   filter(HitOrder %in% c("Top")) %>%
-  select(DATE, SITE, TRTMT, PLOT, SPECIES, HitOrder, CanopyHeight.mm.)
+  select(DATE, SITE, TRTMT, PLOT, SPP, HitOrder, CanopyHeight.mm.)
 
 #Change PLOT to factor for randomness in MCMC
 Height_ALL$PLOT <- as.factor(Height_ALL$PLOT)
 Height_ALL$TRTMT <- as.factor(Height_ALL$TRTMT)
+Height_ALL$DATE <- as.numeric(Height_ALL$DATE)
 
-#Specify dplyr package for group_by and summarise functions 
+#Specify dplyr package for group_by and summarize functions 
 Height_ALL <- Height_ALL %>%
   dplyr::group_by(DATE, TRTMT, PLOT, SITE) %>%
   dplyr::summarize(mean_height = mean(CanopyHeight.mm., na.rm = TRUE)) %>%
   ungroup()
 
-#Convert to data frame before passing to MCMC 
+#Convert to data frame
 Height_ALL_df <- as.data.frame(Height_ALL)
-
-#prior2 <- list(R = list(V = 1, nu = 0.002), 
-               #G = list(G1 = list(V = 1, nu = 1, alpha.mu = 0, alpha.v = 10000), 
-                        #G2 = list(V = 1, nu = 1, alpha.mu = 0, alpha.v = 10000)))
 
 #Using a prior without random variables 
 prior2 <- list(
-  R = list(V = 1, nu = 0.002)) #,
-  #G = list(
-    #G1 = list(V = 1, nu = 2, alpha.mu = 0, alpha.v = 10000),
-            #G2 = list(V = 1, nu = 2, alpha.mu = 0, alpha.v = 10000)))
+  R = list(V = 1, nu = 0.002))
 
 
 #Warning message, "Unknown or unitialized column: 'family'" --> ran with only one or none random variables
-ALL_Height_OTC <- MCMCglmm(mean_height ~ I(DATE - 2022), 
-                             #random = ~ DATE + PLOT, 
+ALL_Height_OTC <- MCMCglmm(mean_height ~ I(DATE - 2022),
                              data = Height_ALL_df[Height_ALL_df$TRTMT == "warming",], 
                              family="gaussian",
                              pr = TRUE, 
@@ -197,7 +168,6 @@ ALL_Height_OTC <- MCMCglmm(mean_height ~ I(DATE - 2022),
                              prior = prior2)
 
 ALL_Height_CTL <- MCMCglmm(mean_height ~ I(DATE-2022), 
-                           #random = ~ DATE+PLOT, 
                            data = Height_ALL_df[Height_ALL_df$TRTMT == "control",], 
                            family ="gaussian", 
                            pr = TRUE, 
@@ -278,12 +248,7 @@ ggplot() +
   xlab("Year")+
   ylab("Mean height (cm)")+
   theme(
-    #axis.text.x=element_blank(),
-    #   axis.title.x=element_blank(),
-    #  axis.title.y=element_blank(),
-    # axis.ticks.x =element_blank(),
     text = element_text(size=9),
-    #legend.position="none",
     plot.margin = unit(c(0,0.5,0,0.5), "cm"))
 
 ggsave("Height_regression.tiff", last_plot(), dpi = 500)
@@ -312,3 +277,4 @@ anova_result <- aov(mean_height ~ DATE * TRTMT, data = Height_ALL)
 
 # Summary of the ANOVA result
 summary(anova_result)
+
